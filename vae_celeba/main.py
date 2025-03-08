7d4b47b85cb181579f56d8e4d8f85451f44f61e8
mydavidallar <davidallar@foxmail.com>
2025-03-08 21:41:43 +0800

    Add files via upload

---
+++ vae_celeba/main.py
@@ -0,0 +1,87 @@
+from time import time
+
+import torch
+import torch.nn.functional as F
+from torchvision.transforms import ToPILImage
+
+from load_celebA import get_dataloader
+from model import VAE
+
+# Hyperparameters
+n_epochs = 10
+kl_weight = 0.00025
+lr = 0.005
+
+
+def loss_fn(y, y_hat, mean, logvar):
+    recons_loss = F.mse_loss(y_hat, y)
+    kl_loss = torch.mean(
+        -0.5 * torch.sum(1 + logvar - mean**2 - torch.exp(logvar), 1), 0)
+    loss = recons_loss + kl_loss * kl_weight
+    return loss
+
+
+def train(device, dataloader, model):
+    optimizer = torch.optim.Adam(model.parameters(), lr)
+    dataset_len = len(dataloader.dataset)
+
+    begin_time = time()
+    # train
+    for i in range(n_epochs):
+        loss_sum = 0
+        index = 0
+        for x in dataloader:
+            index += 1
+            print('current: ', index, '/', len(dataloader), ' ...')
+            x = x.to(device)
+            y_hat, mean, logvar = model(x)
+            loss = loss_fn(x, y_hat, mean, logvar)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            loss_sum += loss
+        loss_sum /= dataset_len
+        training_time = time() - begin_time
+        minute = int(training_time // 60)
+        second = int(training_time % 60)
+        print(f'epoch {i}: loss {loss_sum} {minute}:{second}')
+        torch.save(model.state_dict(), 'output/model.pth')
+
+
+def reconstruct(device, dataloader, model):
+    model.eval()
+    batch = next(iter(dataloader))
+    x = batch[0:1, ...].to(device)
+    output = model(x)[0]
+    output = output[0].detach().cpu()
+    input = batch[0].detach().cpu()
+    combined = torch.cat((output, input), 1)
+    img = ToPILImage()(combined)
+    img.save('tmp.jpg')
+
+
+def generate(device, model):
+    model.eval()
+    output = model.sample(device)
+    output = output[0].detach().cpu()
+    img = ToPILImage()(output)
+    img.save('tmp.jpg')
+
+
+def main():
+    device = 'cuda'
+    dataloader = get_dataloader()
+
+    model = VAE().to(device)
+
+    # Choose the function
+    #train(device, dataloader, model)
+    
+    # If you obtain the ckpt, load it
+    model.load_state_dict(torch.load('output/model.pth', 'cuda'))
+    reconstruct(device, dataloader, model)
+    #generate(device, model)
+
+
+if __name__ == '__main__':
+    main()
